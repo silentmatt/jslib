1b7aec41e5edf1605d2e66f614e8066d45ef811d
Matthew Crumley <email@matthewcrumley.com>
2008-09-27 22:36:38 -0400

    Improved Math.isPrime

--- math.js
+++ math.js
@@ -466,25 +466,25 @@ Number.prototype.isNegative = function() {
  * @return              <code>true</code> if <code>iX</code> is prime
  * @return              <code>false</code> if <code>iX</code> is not prime
  */
-Math.isPrime = function(iX) {
-	if (iX < 2) {
-		return false;
-	}
-
-	if ((iX != 2) && ((iX % 2) == 0)) {
-		return false;
-	}
-	else {
-		var limit = Math.sqrt(iX);
-		for (var i = 3; i <= limit; i += 2) {
-			if ((iX % i) == 0) {
-				return false;
-			}
-		}
-	}
-
-	return true;
+Math.isPrime = function(n) {
+    if (n <= 1) { return false; }
+    else if (n < 4) { return true; } //2 and 3 are prime
+    else if (n % 2 === 0) { return false; }
+    else if (n < 9) { return true; }     //we have already excluded 4,6 and 8.
+    else if (n % 3 === 0) { return false; }
+    else {
+        var r = Math.floor(Math.sqrt(n));
+        var f = 5;
+        while (f <= r) {
+            if (((n % f) === 0) || ((n % (f+2)) === 0)) {
+                return false;
+            }
+            f += 6;
+        }
+    }
+    return true;
 }
+
 
 
 /**
